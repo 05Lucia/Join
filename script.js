009abd31c36f22e917716826c1cfb59a7e0c9c96
CaroWillers <140181592+CaroWillers@users.noreply.github.com>
2024-04-07 20:59:47 +0200

    update Caro's tasks

--- script.js
+++ script.js
@@ -29,4 +29,37 @@ function Templates(template) {
      includeHTML();
 }
 
+//validate password// 
+function validateForm() {
+    let password = document.getElementById("password").value;
+    let confirmPassword = document.getElementById("confirm_password").value;
+    
+    if (password !== confirmPassword) {
+        document.getElementById("message").innerHTML = "Passwords do not match!";
+        return false;
+    } else {
+        document.getElementById("message").innerHTML = ""; 
+        console.log("Form submitted successfully.");  
+    }
+}
+
+// message for successful signup//
+function succesfulSignup() {
+    document.getElementById("signupModal").style.display = "block";
+}
+   
+function closeModal() {
+    document.getElementById("signupModal").style.display = "none";
+    window.location.href = '../Templates/summary.html';
+}
+  
+// closes window on click//
+window.onclick = function(event) {
+    let modal = document.getElementById("signupModal");
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+}
+  
+ 
  
