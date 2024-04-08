157591313015b4abdd51d8f3d4e5f0de0150fa42
Alice Buchholz <alice.buchholz@gmail.com>
2024-04-08 11:08:15 +0200

    button reations for search and add

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 function init() {
     //login page after start animation//
     setTimeout(() => {
-        window.location.href = 'login.html';  
-    }, 1200); 
-    includeHTML(); 
+        window.location.href = 'login.html';
+    }, 1200);
+    includeHTML();
 }
 
+/**
+ * this funktion ist to open a certain Template.
+ */
 async function includeHTML() {
     let includeElements = document.querySelectorAll('[include-html]');
     for (let i = 0; i < includeElements.length; i++) {
@@ -20,26 +23,31 @@ async function includeHTML() {
     }
 }
 
+/**
+ * This funktion is to choes the right template for a certain button
+ * 
+ * @param {string} template - name of the template that should be used
+ */
 function Templates(template) {
     const content = document.getElementById('content');
-    content.innerHTML= '';
+    content.innerHTML = '';
     content.innerHTML = `
     <div include-html="./Templates/${template}.html"> </div>
     `;
-     includeHTML();
+    includeHTML();
 }
 
 //validate password// 
 function validateForm() {
     let password = document.getElementById("password").value;
     let confirmPassword = document.getElementById("confirm_password").value;
-    
+
     if (password !== confirmPassword) {
         document.getElementById("message").innerHTML = "Passwords do not match!";
         return false;
     } else {
-        document.getElementById("message").innerHTML = ""; 
-        console.log("Form submitted successfully.");  
+        document.getElementById("message").innerHTML = "";
+        console.log("Form submitted successfully.");
     }
 }
 
@@ -47,19 +55,19 @@ function validateForm() {
 function succesfulSignup() {
     document.getElementById("signupModal").style.display = "block";
 }
-   
+
 function closeModal() {
     document.getElementById("signupModal").style.display = "none";
     window.location.href = '../Templates/summary.html';
 }
-  
+
 // closes window on click//
-window.onclick = function(event) {
+window.onclick = function (event) {
     let modal = document.getElementById("signupModal");
     if (event.target == modal) {
-      modal.style.display = "none";
+        modal.style.display = "none";
     }
 }
-  
- 
- 
+
+
+
