697cd9373f03ce6999e06a6822579c1237659bae
CaroWillers <140181592+CaroWillers@users.noreply.github.com>
2024-04-09 19:44:08 +0200

    Signup Form Validation

--- JS/register.js
+++ JS/register.js
@@ -1,18 +1,93 @@
-  
-//validate password// 
-function validateForm() {
+//check password Strength//
+function checkPasswordStrength() {
     let password = document.getElementById("password").value;
-    let confirmPassword = document.getElementById("confirm_password").value;
+    let strengthIndicator = document.getElementById("passwordStrengthMessage");
+    let pattern = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;
 
-    if (password !== confirmPassword) {
-        document.getElementById("message").innerHTML = "Passwords do not match!";
-        return false;
+    if (!pattern.test(password)) {
+        strengthIndicator.innerHTML = "Your password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, and one number";
+        strengthIndicator.style.color = "red";
     } else {
-        document.getElementById("message").innerHTML = "";
-        console.log("Form submitted successfully.");
+        strengthIndicator.innerHTML = "Your password strength is ok";
+        strengthIndicator.style.color = "green";
     }
 }
 
+//check confirmed password match//
+function validateConfirmedPassword() {
+    let password = document.getElementById("password").value;
+    let confirmPassword = document.getElementById("confirm_password").value;
+    let message = document.getElementById("passwordMatchMessage"); 
+
+    if (password !== confirmPassword) {
+        message.textContent = "Your passwords do not match";
+        message.style.color = "red";
+    } else {
+        message.textContent = "Your password is confirmed";
+        message.style.color = "green";
+    }
+}
+
+//toggle password visibility//
+function togglePassword(fieldId) {
+    let input = document.getElementById(fieldId);
+    let iconId = fieldId === "password" ? "passwordIcon" : "confirmPasswordIcon";
+    let icon = document.getElementById(iconId);
+    let inputImage = src="./img/img/lock.svg";
+
+    if (input.type === "password") {
+        input.type = "text";
+        icon.src = "./img/img/visibility_off.svg";  
+    } else {
+        input.type = "password";
+        icon.src = "./img/img/visibility.svg";  
+
+        if(input.value === "") {  
+            inputImage.style.display = "block";
+    }
+}
+}
+
+function toggleCheckbox() {
+    let checkboxImage = document.getElementById("checkboxImage");
+    let realCheckbox = document.getElementById("realCheckbox");
+
+    if (realCheckbox.checked) {
+        checkboxImage.src = "../img/Rectangle5.svg";  
+        realCheckbox.checked = false;
+    } else {
+        checkboxImage.src = "../img/img/checked.svg";  
+        realCheckbox.checked = true;
+    }
+}
+
+// check if Privacy Policy was accepted// 
+function checkPrivacyPolicy() { 
+    let realCheckbox = document.getElementById("realCheckbox");
+    if (!realCheckbox.checked) {
+        alert("Please accept the Privacy Policy conditions");
+        return false;
+    } 
+}
+
+function checkPrivacyPolicy() { 
+    let checkboxImage = document.getElementById("checkboxImage").checked; 
+    if (!checkboxImage) {
+        alert("Please accept the Privacy Policy conditions");
+        return false;
+    }
+    return true;  
+}
+
+
+function validateForm() {      
+    if (!checkPasswordStrength() || !validateConfirmedPassword() || !checkPrivacyPolicy()) {
+        return false;
+    }
+    successfulSignup();
+}
+
+ 
 // message for successful signup//
 function successfulSignup() {
     document.getElementById("signupModal").style.display = "block";
@@ -31,3 +106,6 @@ window.onclick = function (event) {
     }
 }
 
+
+
+ 
