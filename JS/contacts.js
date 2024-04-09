f957c5a63a314efa8272dce3bda61153de8a2c95
Vitali Rudi <159520121+Vitali21MBC@users.noreply.github.com>
2024-04-09 22:43:49 +0200

    Contacts JS Update
    - edit Contact functional
    - delete Contact functional
    - new Contacts get a avatar Color when created

--- JS/contacts.js
+++ JS/contacts.js
@@ -1,8 +1,11 @@
+let avatarColors = ["rgb(255,122,0)", "rgb(255,70,70)", "rgb(147,39,255)", "rgb(110,82,255)", "rgb(252,113,255)", "rgb(255,187,43)", "rgb(31,215,193)", "rgb(70,47,138)"]
+
 let contacts = [
     {
         "name": "Anton",
         "surname": "Mayer",
         "initials": "AM",
+        "avatarColor": "rgb(255,122,0)",
         "email": "anton@gmail.com",
         "phone": "+49 1111 111 11 1",
         "category": "A"
@@ -11,6 +14,7 @@ let contacts = [
         "name": "Albert",
         "surname": "Gerdes",
         "initials": "AG",
+        "avatarColor": "rgb(255,70,70)",
         "email": "albert@gmail.com",
         "phone": "+49 2222 222 22 2",
         "category": "A"
@@ -19,6 +23,8 @@ let contacts = [
         "name": "Aaron",
         "surname": "Brier",
         "initials": "AB",
+        "initials": "AG",
+        "avatarColor": "rgb(147,39,255)",
         "email": "aaron@gmail.com",
         "phone": "+49 3333 333 33 3",
         "category": "A"
@@ -27,6 +33,7 @@ let contacts = [
         "name": "Britta",
         "surname": "Zielke",
         "initials": "BZ",
+        "avatarColor": "rgb(110,82,255)",
         "email": "b.zielke@gmail.com",
         "phone": "+49 4444 444 44 4",
         "category": "B"
@@ -35,6 +42,7 @@ let contacts = [
         "name": "Carsten",
         "surname": "Schmidt",
         "initials": "CS",
+        "avatarColor": "rgb(252,113,255)",
         "email": "carsten.schmidt@gmail.com",
         "phone": "+49 5555 555 55 5",
         "category": "C"
@@ -43,6 +51,7 @@ let contacts = [
         "name": "Bernt",
         "surname": "Saathoff",
         "initials": "BS",
+        "avatarColor": "rgb(255,187,43)",
         "email": "bernt.s@gmail.com",
         "phone": "+49 6666 666 66 6",
         "category": "B"
@@ -51,11 +60,22 @@ let contacts = [
         "name": "Caroline",
         "surname": "Tabeling",
         "initials": "CT",
+        "avatarColor": "rgb(31,215,193)",
         "email": "caroline@gmail.com",
         "phone": "+49 7777 777 77 7",
         "category": "C"
     }
 ]
+
+
+function findIndexByNameSurname(contacts, firstName, lastName) {
+    for (let i = 0; i < contacts.length; i++) {
+        if (contacts[i].name === firstName && contacts[i].surname === lastName) {
+            return i; // Index des gefundenen Eintrags
+        }
+    }
+    return -1; // Falls kein Eintrag gefunden wurde
+}
 
 
 function init() {
@@ -108,7 +128,7 @@ function renderContacts(categories) {
             categories[initial].forEach(contact => {
                 contactListHTML += `
                     <div class="contact" onclick="openContactInfo(${index})"> <!-- Index übergeben -->
-                        <div class="contactAvatar">
+                        <div class="contactAvatar" style="background-color: ${contact.avatarColor};">
                             ${contact.initials}
                         </div>
                         <div class="contactNameAndEmail">
@@ -169,6 +189,9 @@ function createContact() {
         firstName = capitalizeFirstLetter(firstName);
         lastName = capitalizeFirstLetter(lastName);
 
+        // Wählen Sie eine zufällige Farbe aus dem avatarColors-Array aus
+        let randomColor = avatarColors[Math.floor(Math.random() * avatarColors.length)];
+
         // Erstellen Sie das Objekt für den neuen Kontakt
         let newContact = {
             "name": firstName,
@@ -176,19 +199,25 @@ function createContact() {
             "initials": firstName.charAt(0).toUpperCase() + lastName.charAt(0).toUpperCase(),
             "email": email,
             "phone": phone,
-            "category": firstName.charAt(0).toUpperCase()
+            "category": firstName.charAt(0).toUpperCase(),
+            "avatarColor": randomColor // Fügen Sie die zufällige Farbe hinzu
         };
 
         // Fügen Sie den neuen Kontakt zum Array hinzu
         contacts.push(newContact);
 
-        let index = contacts.length - 1;
-        openContactInfo(index);
+        // let index = contacts.length - 1;
+        // openContactInfo(index);
 
-        showContactCreatedPopUp();
+        // showContactCreatedPopUp();
 
         // Optional: Aktualisieren Sie die Kontaktliste auf der Seite
         init();
+
+        let contactIndex = findIndexByNameSurname(contacts, firstName, lastName);
+        openContactInfo(contactIndex);
+
+        showContactCreatedPopUp();
 
         // Optional: Leeren Sie die Eingabefelder
         document.getElementById("editContactName").value = "";
@@ -218,7 +247,7 @@ function showContactEditDeleteMenu(index) {
                                                                                         <img src="./img/editContactIcon.svg">
                                                                                         <span>Edit</span>
                                                                                     </div>
-                                                                                    <div class="deleteContact">
+                                                                                    <div class="deleteContact" onclick="deleteContact(${index})">
                                                                                         <img src="./img/deleteContactIcon.svg">
                                                                                         <span>Delete</span>
                                                                                     </div>
@@ -232,6 +261,19 @@ function showContactEditDeleteMenu(index) {
 
 }
 
+function deleteContact(index) {
+    // Lösche den Kontakt aus dem Array
+    contacts.splice(index, 1);
+
+    // Aktualisiere die Anzeige
+    init();
+    // renderContacts(categorizedContacts);
+
+    // Verstecke das Bearbeitungsmenü
+    hideContactEditDeleteMenu();
+    closeContactInfo();
+}
+
 function hideContactEditDeleteMenu() {
     document.getElementById('editContactMenuContainer').classList.remove('showEditContactMenu');
 }
@@ -241,7 +283,7 @@ function showEditContact(index) {
     hideContactEditDeleteMenu();
     document.getElementById('addEditContact').classList.add('showAddEditContactContainer');
     document.getElementById('addAndEditContactHeadline').innerHTML = 'Edit contact';
-    document.getElementById('avatarIcon').style.backgroundColor = 'rgba(255, 122, 0, 1)';
+    document.getElementById('avatarIcon').style.backgroundColor = `${contacts[index]['avatarColor']}`;
     document.getElementById('avatarIcon').innerHTML = `${contacts[index]['initials']}`;
     var inputNameField = document.getElementById('editContactName');
     var nameToShow = `${contacts[index]['name']} ${contacts[index]['surname']}`;
@@ -288,10 +330,13 @@ function updateContact(index) {
         contacts[index].phone = phone;
         contacts[index].category = firstName.charAt(0).toUpperCase();
 
-        openContactInfo(index);
+        // openContactInfo(index);
 
         // Optional: Aktualisieren Sie die Kontaktliste auf der Seite
         init();
+
+        let contactIndex = findIndexByNameSurname(contacts, firstName, lastName);
+        openContactInfo(contactIndex);
 
         // Optional: Leeren Sie die Eingabefelder
         document.getElementById("editContactName").value = "";
@@ -311,7 +356,7 @@ function openContactInfo(index) {
     console.log("Detailansicht für Kontakt:", contact);
     document.getElementById('contactInfoContactDetails').innerHTML = /*html*/`
                     <div class="contactInfoAvatarAndName">
-                        <div class="contactInfoAvatar">
+                        <div class="contactInfoAvatar" style="background-color: ${contact.avatarColor};">
                             ${contact['initials']}
                         </div>
                         <div class="contactInfoName">
@@ -327,7 +372,7 @@ function openContactInfo(index) {
                             <p>Phone</p>
                             <span>${contact['phone']}</span>
                         </div>
-                    </div>   
+                    </div>
             `;
 
     document.getElementById('editContactButtonContainer').innerHTML = /*html*/`
