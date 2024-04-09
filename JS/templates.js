4e88cc7bdef841e3391ad056a0078e2e9bb748d2
Alice Buchholz <alice.buchholz@gmail.com>
2024-04-09 10:29:57 +0200

    card js fist implement

--- JS/templates.js
+++ JS/templates.js
@@ -1,21 +1,25 @@
-function cardTemplate() {
+function cardTemplate(card) {
     return `
-    <div class="card">
-        <div class="category-card">${paceholder}]</div>
+    <div draggable="true" ondragstart="startDraging(${card.id})" class="card">
+        <div class="category-card">${card.category}</div>
         <div class="card-text">
-            <h3>${paceholder}</h3>
-            <p>${paceholder}</p>
+            <h3>${card.titel}</h3>
+            <p>${card.description}</p>
             <!-- dont show more than 2 lines? JS? -->
         </div>
         <div class="progressbar-area">
             <div class="progressbar">
-                <div class="progress-color" style="width: ${paceholder};"></div>
-        </div>
-        <p>${p}/${p} Subtasks</p>
+                <div class="progress-color" style="width:${100}%;"></div>
+            </div>
+            <p>${1}/${card.subtasks.length} Subtasks</p>
         </div>
         <div class="icons-area">
-            <div>${paceholder}</div>
-            <img src="${paceholder}" alt="">
+            <div class="initial-card-container">
+                <div class="user-initals-card" id="user-initals">TD</div>
+                <div class="user-initals-card overlap" id="user-initals">G</div>
+                <div class="user-initals-card overlap" id="user-initals">AB</div>
+            </div>
+            <img src="${card.priority}" alt="">
         </div>
     </div>
     `;
--- script.js
+++ script.js
@@ -28,12 +28,94 @@ async function includeHTML() {
  * 
  * @param {string} template - name of the template that should be used
  */
-function Templates(template) {
+async function Templates(template) {
     const content = document.getElementById('content');
     content.innerHTML = '';
     content.innerHTML = `
     <div include-html="./Templates/${template}.html"> </div>
     `;
-    includeHTML();
+    await includeHTML();
 }
 
+let cards =[
+    {
+        id: 0,
+        place: 'todo',
+        category : 'js',
+        titel: 'Test',
+        description: 'irgend was ....',
+        dueDate: '',
+        subtasks: {
+            1: {
+                name: 'testing',
+                done: true
+            },
+            2: {
+                name: 'sonmthing',
+                done: false
+            }
+        },
+        assigned: {
+            users: ['Alice Buchholz', 'Guest', 'Test Dummy']
+        },
+        priority : ''
+
+    }
+]
+
+async function lodeBoard (){
+    await Templates('board');
+    updateCads()
+}
+
+let currentDraggedElement;
+
+function updateCads() {
+    let todo = cards.filter(t => t['place'] == 'todo');
+
+    document.getElementById('todo').innerHTML = '';
+
+    for (let index = 0; index < todo.length; index++) {
+        const card = todo[index];
+        document.getElementById('todo').innerHTML += cardTemplate(card);
+    }
+
+    let progress = cards.filter(t => t['place'] == 'progress');
+
+    document.getElementById('progress').innerHTML = '';
+
+    for (let index = 0; index < progress.length; index++) {
+        const card = progress[index];
+        document.getElementById('progress').innerHTML += cardTemplate(card);
+    }
+
+    let feedback = cards.filter(t => t['place'] == 'feedback');
+
+    document.getElementById('feedback').innerHTML = '';
+
+    for (let index = 0; index < feedback.length; index++) {
+        const card = feedback[index];
+        document.getElementById('feedback').innerHTML += cardTemplate(card);
+    }
+
+    let done = cards.filter(t => t['place'] == 'done');
+
+    document.getElementById('feedback').innerHTML = '';
+
+    for (let index = 0; index < done.length; index++) {
+        const card = done[index];
+        document.getElementById('done').innerHTML += cardTemplate(card);
+    }
+}
+
+function startDraging(id) {
+    currentDraggedElement = id;
+}
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drop(event) {
+
+}
